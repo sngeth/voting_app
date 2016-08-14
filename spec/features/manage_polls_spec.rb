3f2231905ca80904eba35f3f1758b9424f417a63
Sid Ngeth <sngeth@gmail.com>
2016-08-13 21:02:46 -0400

    View created poll with choices

--- spec/features/manage_polls_spec.rb
+++ spec/features/manage_polls_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Poll management", type: :feature do
-  scenario "User creates a new poll" do
+  scenario "User can create and view new poll" do
     visit "/polls/new"
 
     fill_in "Title", with: "What is your favorite brand?"
@@ -10,5 +10,8 @@ RSpec.feature "Poll management", type: :feature do
     click_button "Create Poll"
 
     expect(page).to have_text("Poll was successfully created.")
+    expect(page).to have_content("What is your favorite brand")
+    expect(page).to have_content("Coke")
+    expect(page).to have_content("Pepsi")
   end
 end
