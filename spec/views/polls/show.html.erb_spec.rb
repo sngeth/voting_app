bb49453f7e5ee58ddc058afb144c7ac456b6a9f6
Sid Ngeth <sngeth@gmail.com>
2016-08-13 21:52:34 -0400

    Fix poll show view spec

--- spec/views/polls/show.html.erb_spec.rb
+++ spec/views/polls/show.html.erb_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe "polls/show", type: :view do
     @poll = assign(:poll, Poll.create!(
       :title => "Title"
     ))
+
+    @poll.choices.create(name: "Choice 1")
+    @poll.choices.create(name: "Choice 2")
   end
 
   it "renders attributes in <p>" do
